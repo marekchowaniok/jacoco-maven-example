b3762e530eca2fda333889f99d5e1735edf33b35
Marek Chowaniok <marek.chowaniok@gmail.com>
2014-03-18 22:45:18 +0100

    init

---
+++ src/main/java/aaaa/App.java
@@ -0,0 +1,20 @@
+package aaaa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+	private void aaa() {
+		if("xx".length() > 7) {
+			
+		}
+	}
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
